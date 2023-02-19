36a072d9d6bdde526853a4c86afac8769f072086
PpNutthanon <nutthanon.hw@gmail.com>
2023-02-19 21:20:43 +0700

    Learn more 2 topics
    Regarding Type Function and Type of Data in Python

---
+++ BornToDEV/3) Display.py
@@ -0,0 +1,8 @@
+# type => ฟังก์ชันเอาไว้ทำการตรวจสอบข้อมูลภายในวงเล็บว่า เป็นข้อมูลประเภทไหน
+print(type("Hello World")) #*String
+print(type(6)) #? Integer
+print(type(6.5)) #TODO Float
+print(type(2j)) #! Complex
+print(type(True)) # Boolean
+print(type(False)) # Boolean
+print(type(10+3.5)) #? Float
