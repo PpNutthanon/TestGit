0535057aae6c413ac32632bc6a81bcb7ca74af56
PpNutthanon <nutthanon.hw@gmail.com>
2023-02-20 11:12:29 +0700

    Math & Boolean Operations

---
+++ BornToDEV/12) MathOp.py
@@ -0,0 +1,11 @@
+# Mathematics Operations => + - * /
+x = 10
+y = 5
+z = x+y
+print(z)
+
+money = 1000
+daily_amount = 200
+spend = 300
+remain = money + ((daily_amount-spend)*30)
+print(remain)
---
+++ BornToDEV/13) BoolOp.py
@@ -0,0 +1,10 @@
+# Boolean Operators => and, or, <, >, <=, >=, =
+x = True
+y = False
+z = x > y
+a = x < y
+print(z,a)
+
+age = 10
+print("You overage?")
+print(age>18)
