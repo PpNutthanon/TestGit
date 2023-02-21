16ba966ff9261092aa6a8ab5029eafd4fbf0d3b7
PpNutthanon <nutthanon.hw@gmail.com>
2023-02-21 17:25:32 +0700

    Create 16) Input.py

---
+++ BornToDEV/16) Input.py
@@ -0,0 +1,9 @@
+x = input("Hi") #! คำสั่ง input จะให้ค่าออกมาเป็น string ไม่ว่าเราจะใส่ ตัวอักษรหรือตัวเลขก็ตาม
+print("Hi",type(x),x)
+name = input("First name :")
+lastname = input("Lastname :")
+print("Hello",name,lastname)
+
+number1 = input("Number 1 :")
+number2 = input("Number 2 :")
+print(float(number1)+float(number2))
