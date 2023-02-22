078de5033e4e95d581f7675de6bdd38dabf36788
PpNutthanon <nutthanon.hw@gmail.com>
2023-02-23 01:41:33 +0700

    --

--- BornToDEV/8) String.py
+++ BornToDEV/8) String.py
@@ -2,3 +2,6 @@ print("Hello PP")
 print(type("Hello PP"))
 print("Hello" + "World")
 print("Hello" *3) #! ภาษา Python สามารถคูณข้อความได้
+word = input() + " "
+times= int(input())
+print(word*times)
