ff777efc9a43f33e6bf5fd10ca6bd0284daf5407
PpNutthanon <nutthanon.hw@gmail.com>
2023-02-23 02:00:27 +0700

    Create 19) Login.py

---
+++ BornToDEV/19) Login.py
@@ -0,0 +1,6 @@
+Username = input("Username :")
+Password = input("Password :")
+if Username== "Pp" and Password== "000ppp":
+    print("You can login")
+else:
+    print("Username or Password incorrect")
