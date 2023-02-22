a7b8420dd5b8ae00cfd07685b92e96b1815d7e0d
PpNutthanon <nutthanon.hw@gmail.com>
2023-02-23 01:56:09 +0700

    Create 18) IfElse.py

---
+++ BornToDEV/18) IfElse.py
@@ -0,0 +1,11 @@
+# เงื่อนไข If Else จะอยู่ในข้อมูลแบบ Boolean
+if True:
+    print("Hello Pp")
+    print("Hello Condition")
+
+money = 150
+if money>300:
+    print("Eat KFC")
+else:
+    print("Mama :(")
+print("ByeBye")
