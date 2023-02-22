bbfeb3deb0cf86f2b8da7cc69540c6dfe511673e
PpNutthanon <nutthanon.hw@gmail.com>
2023-02-23 02:14:32 +0700

    --

---
+++ BornToDEV/CP3/20) Elif.py
@@ -0,0 +1,9 @@
+#elif ทำงานก็ต่อเมื่อเงื่อนไขด้านบนเป็นเท็จ
+if False:
+    print("First Condition")
+elif True:
+    print("Second Condition")
+elif True:
+    print("Third Condition")
+else:
+    print("Else Section")
