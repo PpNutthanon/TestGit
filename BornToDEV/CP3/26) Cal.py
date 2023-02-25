6f16648bc898cea7ce76993535ed07a27a810cb4
PpNutthanon <nutthanon.hw@gmail.com>
2023-02-25 14:07:57 +0700

    Update 26) Cal.py

--- BornToDEV/CP3/26) Cal.py
+++ BornToDEV/CP3/26) Cal.py
@@ -4,3 +4,8 @@ for i in range(round):
     number = float(input("Enter the "+str(i+1)+" number :"))
     sum = sum + number
 print("The Sum of the Number :",sum)
+
+start = int(input("Start:"))
+step = int(input("Step:"))
+for  i in range(5):
+    print(start+step*i,end="")
