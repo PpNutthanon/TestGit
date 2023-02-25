25e96366086a4862e128b600800d2901d697f447
PpNutthanon <nutthanon.hw@gmail.com>
2023-02-25 13:43:04 +0700

    Create 26) Cal.py

---
+++ BornToDEV/CP3/26) Cal.py
@@ -0,0 +1,6 @@
+sum = 0
+round = int(input("Enter the round of sum :"))
+for i in range(round):
+    number = float(input("Enter the number :"))
+    sum = sum + number
+    print("The Sum of the Number :",sum)
