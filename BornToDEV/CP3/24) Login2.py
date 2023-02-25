660037b2459003693bea4368566679559f30a6c5
PpNutthanon <nutthanon.hw@gmail.com>
2023-02-25 13:23:11 +0700

    --

--- BornToDEV/CP3/24) Login2.py
+++ BornToDEV/CP3/24) Login2.py
@@ -5,5 +5,5 @@ while Username != "admin" or Password != "123" :
     Password = input("Password :")
     if Username != "admin" or Password != "123":
         print("Incorrect username or password")
-    elif Username == "admin" and Password == "123":
-        print("You can login")
+   
+print("You can login")
