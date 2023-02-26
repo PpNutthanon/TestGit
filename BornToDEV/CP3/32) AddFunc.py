9031ee65bd531cd775082f7463035926aa3335a0
PpNutthanon <nutthanon.hw@gmail.com>
2023-02-26 14:16:17 +0700

    Create 32) AddFunc.py

---
+++ BornToDEV/CP3/32) AddFunc.py
@@ -0,0 +1,20 @@
+def addNumber(x,y):
+    print("Sum of numbers :",x+y)
+
+def diffNumber(x,y):
+    print("Diff of numbers :",x-y)
+
+def productNumber(x,y):
+    print("Product of numbers :",x*y)
+
+def divideNumber(x,y):
+    print("Divide of numbers :",x/y)
+addNumber(50,21)
+
+def MathOp(x,y):
+    print("Sum of numbers :",x+y)
+    print("Diff of numbers :",x-y)
+    print("Product of numbers :",x*y)
+    print("Divide of numbers :",x/y)
+
+MathOp(50,21)
