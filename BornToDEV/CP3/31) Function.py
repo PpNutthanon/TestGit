064e39d8e405b37fb644b102914365597d694210
PpNutthanon <nutthanon.hw@gmail.com>
2023-02-26 14:08:16 +0700

    Update 31) Function.py

--- BornToDEV/CP3/31) Function.py
+++ BornToDEV/CP3/31) Function.py
@@ -2,10 +2,13 @@
 def SayHi():
     print("Hello World!")
     SayName(name)
+    print("Wooooo!")
 
 def SayName(name):
     print("Hello",name)
-
+    print("Haha")
+    
 name  = input("Enter name: ")
 SayHi() #TODO เรียกใช้งานฟังก์ชัน SayHi
 SayName(name)
+
