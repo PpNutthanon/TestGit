f9d74c1dcd017b763dea85011c2ab6091f5fc001
PpNutthanon <nutthanon.hw@gmail.com>
2023-02-27 21:17:27 +0700

    --

--- BornToDEV/CP3/35) Return3.py
+++ BornToDEV/CP3/35) Return3.py
@@ -1,20 +1,32 @@
-User = input("Username :")
-Password = input("Password :")
-if User == "admin" and Password == "PP":
-    print("Done")
+def login():
+    User = input("Username :")
+    Password = input("Password :")
+    if User == "admin" and Password == "PP":
+        return True
+    else:
+        return False
+    
+def showmenu():
     print("-----Pp Shop-----")
     print("1) VAT Calculator")
     print("2) Price Calculator")
+
+def menuselect():
     select = int(input("Enter number : "))
-    if select == 1:
-        price = float(input("Enter the price :"))
-        VAT = 0.07
-        result = price*(1+VAT)
-        print(result)
-    elif select == 2:
-        price1 = int(input("Enter the price :"))
-        price2 = int(input("Enter the price :"))
-        print(price1+price2)
+    return select
+
+def VAT(totalprice):
+    VAT = 0.07
+    result = totalprice*(1+VAT)
+    return result
+
+def Price():
+    price1 = int(input("Enter the price :"))
+    price2 = int(input("Enter the price :"))
+    return VAT(price1+price2)
+
+    
+print(Price())
 
 #Login
 #Show Menu
