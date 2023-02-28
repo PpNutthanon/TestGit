f518416a104075739086a90b72a62881b4e57184
PpNutthanon <nutthanon.hw@gmail.com>
2023-03-01 01:20:54 +0700

    Update 38) String3.py

--- BornToDEV/CP3/38) String3.py
+++ BornToDEV/CP3/38) String3.py
@@ -1,3 +1,7 @@
 nickname = input("Enter your nickname: ")
 weight = float(input("Enter your weight:"))
-print("Hello "+ nickname +" and weight",weight)
+print("Hello "+nickname+" and weight is",weight,"kg")
+print("Hello %s and weight is %d" % (nickname, weight)) 
+# %s => String
+# %d => Integer
+# %f => Float
