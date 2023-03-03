414f565a0c925064baee6e366105bfa6168c5ed0
PpNutthanon <nutthanon.hw@gmail.com>
2023-03-03 23:03:01 +0700

    ---

---
+++ BornToDEV/CP3/41) List.py
@@ -0,0 +1,10 @@
+# List => ข้อมูลต่างๆที่ถูกนำเข้ามาเรียงเป็นลำดับ (เพิ่มข้อมูลลงไปได้)
+list1 = [0,2,4,6,8]
+print(list1)
+for i in list1:
+    print(i)
+
+customerlist = ["Nutthanon","Pp","Much"]
+print(customerlist[:2])
+print(customerlist[1])
+
---
+++ BornToDEV/CP3/42) ListFunc.py
@@ -0,0 +1,9 @@
+friends = ["Nutthanon","Pp","Much"]
+print(friends)
+friends[2] = "New" #TODO Update Lists เปลี่ยนข้อมูล
+friends.append("Fobe") #TODO เพิ่มข้อมูลเข้าไปใน list
+print(friends)
+friends.remove("Pp") #Todo ลบข้อมูลใน List
+print(friends)
+del friends[0]
+print(friends)
