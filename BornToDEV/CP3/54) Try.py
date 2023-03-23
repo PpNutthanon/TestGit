ddc6e29a14972d0a607b8fa51d6ef86c64b87383
PpNutthanon <nutthanon.hw@gmail.com>
2023-03-23 16:21:08 +0700

    --

--- BornToDEV/CP3/54) Try.py
+++ BornToDEV/CP3/54) Try.py
@@ -1,4 +1,10 @@
 try:
-    pass
+    a = int(input("Enter Number: "))
+    b = int(input("Enter Number: "))
+    print(a/b)
+except ValueError:
+    print("Error Can Input Only Number")
+except ZeroDivisionError:
+    print("Error You Can't Enter Zero in Denominator")
 except:
-    pass
+    print("Error")
---
+++ BornToDEV/CP3/55) Assert.py
@@ -0,0 +1,4 @@
+#Symatic Error => ข้อมูลที่ผิดพลาดจากคนเขียนเอง(เช่น เขียนลบเป็นบวก)
+for i in range(10):
+    print(i)
+    assert i <5,"Error"
