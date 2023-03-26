455b64c73b64ed15011f3cb17d2ba14ccea8fef7
PpNutthanon <nutthanon.hw@gmail.com>
2023-03-26 12:35:32 +0700

    Assert

--- BornToDEV/CP3/55) Assert.py
+++ BornToDEV/CP3/55) Assert.py
@@ -1,4 +1,6 @@
 #Symatic Error => ข้อมูลที่ผิดพลาดจากคนเขียนเอง(เช่น เขียนลบเป็นบวก)
+
+#assert => ตรวจสอบเงื่อนไข
 for i in range(10):
     print(i)
-    assert i <5,"Error"
+    assert i <5,"Error" #ถ้าจริงไม่แสดงอะไร ถ้าเท็จให้แสดง "Error"
