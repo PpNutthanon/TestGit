92bce8b3518732084f2b25ef4591f9d040441184
PpNutthanon <nutthanon.hw@gmail.com>
2023-03-26 13:08:18 +0700

    --

---
+++ BornToDEV/CP3/57) Write-File.py
@@ -0,0 +1,4 @@
+file = open("demo.txt","a") #a = add ข้อความ
+file1= open("demo.txt","w") #w = ทับข้อความเดิม
+file2 = open("demo1.txt","x") #x = การสร้างไฟล์
+file.write("Haha from the next lecture")
