40aa010964b87f53b217e5b16648492b225fb7f1
PpNutthanon <nutthanon.hw@gmail.com>
2023-03-29 17:13:45 +0700

    --

--- BornToDEV/CP3/67) Event-Driven.py
+++ BornToDEV/CP3/67) Event-Driven.py
@@ -1,2 +1,17 @@
 from tkinter import *
+
+def leftclick(event):
+    print("Left Click")
+
+def doubleclick(event):
+    print("Double Click")
+
 main = Tk()
+button = Button(main, text="My Button", width=10, fg="White", bg="Black", font=("Didot", 10))
+button.pack()
+button.bind("<Button-1>", leftclick)
+button.bind("<Double-1>", doubleclick)
+
+main.mainloop()
+
+#TODO More Function About Event-Driven : https://python-course.eu/tkinter/events-and-binds-in-tkinter.php
