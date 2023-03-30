3428fc0d52b317a560ad62c23d36c52a644c9888
PpNutthanon <nutthanon.hw@gmail.com>
2023-03-30 13:24:45 +0700

    Tkinter with GUI

--- BornToDEV/CP3/68) Test-GUI.py
+++ BornToDEV/CP3/68) Test-GUI.py
@@ -4,20 +4,8 @@ mainWindow = Tk()
 
 def lefclickbutton(event):
     BMI = (float(textbox_Weight.get()))/(math.pow(float(textbox_Height.get())/100,2))
-    BMI = round(BMI,2)
     print("Your BMI is : ",BMI)
     label_BMI.configure(text=BMI)
-#Todo Show Results
-    if BMI >30:
-        label_Results.configure(text="Extremely Overweight")
-    elif BMI>25 and BMI<29.9:
-        label_Results.configure(text="Overweight")
-    elif BMI>23 and BMI<24.9:
-        label_Results.configure(text="Overweight(Mild)")
-    elif BMI>18.6 and BMI<22.9:
-        label_Results.configure(text="Normal")
-    else:
-        label_Results.configure(text="Underweight")
     
 
 #! ถ้าใส่ .grid() ต่อจาก textbox_Height หริอ textbox_Weight ไปเลย จะไม่สามารถนำข้อมูลออกมาใช้ได้ => ต้องไปเขียนแยกบรรทัด(บรรทัดที่9) ถึงจะเอาข้อมูลไปใช้ต่อได้
@@ -43,11 +31,9 @@ calculationButton.grid(row=2)
 #Todo Event-Driven Pragramming 
 calculationButton.bind("<Button-1>",lefclickbutton)
 
-#Todo Create Label BMI and Results
+#Todo
 label_BMI = Label(mainWindow,text="")
 label_BMI.grid(row=2,column=1)
-label_Results = Label(mainWindow,text="")
-label_Results.grid(row=2,column=2)
 
 #Todo Show the Program
 mainWindow.mainloop()
