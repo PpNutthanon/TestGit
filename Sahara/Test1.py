cba42a2d7b5614285538d8b78ebd01febc815890
PpNutthanon <nutthanon.hw@gmail.com>
2023-04-17 21:59:57 +0700

    --

--- Sahara/Test1.py
+++ Sahara/Test1.py
@@ -8,10 +8,9 @@ print("---Enter variables input (number)---")
 for i, var in enumerate(variables, 1):
     print(f" [{i}] : {var.upper()}")
 
-def find_values(var_index):
-    value = float(input(f"Enter {variables[var_index-1].upper()} value: "))
-    less_than_mask = df[variables[var_index-1]] < value
-    greater_than_mask = df[variables[var_index-1]] > value
+def find_values(var_index, input_value):
+    less_than_mask = df[variables[var_index-1]] < input_value
+    greater_than_mask = df[variables[var_index-1]] > input_value
     
     if less_than_mask.any():
         closest_less_than = df[less_than_mask].iloc[-1]
@@ -23,24 +22,28 @@ def find_values(var_index):
     else:
         closest_greater_than = None
         
-    exact_match = df[df[variables[var_index-1]] == value]
+    exact_match = df[df[variables[var_index-1]] == input_value]
     return exact_match, closest_less_than, closest_greater_than
 
 data = int(input("Enter variables you want to input: "))
-
+value = float(input(f"Enter {variables[data-1].upper()} value: "))
 
 if 1 <= data <= 6:
-    exact_values, values_less, values_greater = find_values(data)
+    exact_values, values_less, values_greater = find_values(data, value)
     
     if len(exact_values) == 0:
         print("No exact match found. Closest values:")
         print("\nRow before:", values_less)
         print("\nRow after:", values_greater)
         values_less   = values_less.tolist()
-        value_greater = values_greater.tolist()
-        if data == 1:
-            ratio = value #Todo: ใส่สูตร
-        #? ถ้าใส่ input t, h, pr, u, vr, s จะเอาตัวแปรไหนมาเป็น references ในการคำนวณ 
+        values_greater = values_greater.tolist()
+        interpolated_values = []
+        ratio = (value - values_less[data-1])/(values_greater[data-1] - values_less[data-1])
+        for i in range(len(variables)):
+            interpolated = values_less[i] + (ratio * (values_greater[i] - values_less[i]))
+            interpolated_values.append(round(interpolated,3))
+        print(interpolated_values)
+        #? Error ตรงที่ Vr ไม่ Exact Value เอามาคำนวณแล้วได้ค่าแปลกๆ
         #Todo (tb - tl)/(tg-tl) = (x - tl)/(tg-tl)
     else:
         print("Exact match found:")
--- Sahara/pp.py
+++ Sahara/pp.py
@@ -8,10 +8,9 @@ print("---Enter variables input (number)---")
 for i, var in enumerate(variables, 1):
     print(f" [{i}] : {var.upper()}")
 
-def find_values(var_index):
-    value = float(input(f"Enter {variables[var_index-1].upper()} value: "))
-    less_than_mask = df[variables[var_index-1]] < value
-    greater_than_mask = df[variables[var_index-1]] > value
+def find_values(var_index, input_value):
+    less_than_mask = df[variables[var_index-1]] < input_value
+    greater_than_mask = df[variables[var_index-1]] > input_value
     
     if less_than_mask.any():
         closest_less_than = df[less_than_mask].iloc[-1]
@@ -23,23 +22,30 @@ def find_values(var_index):
     else:
         closest_greater_than = None
         
-    exact_match = df[df[variables[var_index-1]] == value]
+    exact_match = df[df[variables[var_index-1]] == input_value]
     return exact_match, closest_less_than, closest_greater_than
 
 data = int(input("Enter variables you want to input: "))
-
+value = float(input(f"Enter {variables[data-1].upper()} value: "))
 
 if 1 <= data <= 6:
-    exact_values, values_less, values_greater = find_values(data)
+    exact_values, values_less, values_greater = find_values(data, value)
     
     if len(exact_values) == 0:
         print("No exact match found. Closest values:")
         print("\nRow before:", values_less)
         print("\nRow after:", values_greater)
         values_less   = values_less.tolist()
-        value_greater = values_greater.tolist()
-        if data == 1:
-            ratio = value #Todo: ใส่สูตร
+        values_greater = values_greater.tolist()
+        ratio = (value - values_less[data-1])/(values_greater[data-1] - values_less[data-1])
+        a = values_less[0] + (ratio * (values_greater[0] - values_less[0]))
+        b = values_less[1] + (ratio * (values_greater[1] - values_less[1]))
+        c = values_less[2] + (ratio * (values_greater[2] - values_less[2]))
+        d = values_less[3] + (ratio * (values_greater[3] - values_less[3]))
+        e = values_less[4] + (ratio * (values_greater[4] - values_less[4]))
+        f = values_less[5] + (ratio * (values_greater[5] - values_less[5]))
+        final_value = [a,b,c,d,e,f]
+        print(final_value)
         #? ถ้าใส่ input t, h, pr, u, vr, s จะเอาตัวแปรไหนมาเป็น references ในการคำนวณ 
         #Todo (tb - tl)/(tg-tl) = (x - tl)/(tg-tl)
     else:
